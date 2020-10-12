db9dfcf881a4a7b7dbaeaf79c71cef93c0597de6
Michael Jee <michaeldanieljee@gmail.com>
2020-10-11 22:20:08 -0700

    weird error

--- my-worker/index.js
+++ my-worker/index.js
@@ -1,16 +1,36 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
 
-/*var array = '{ "data": [' +
-  '{"name":"name0", "url":"https://linkurl/0"},' +
-  '{"name":"name1", "url":"https://linkurl/1"},' +
-  '{"name":"name2", "url":"https://linkurl/2"} ]}';*/
 var array = new Array();
+
+class TitleHandler
+{
+  constructor(attributeName) {
+    this.attributeName = attributeName
+  }
+  
+  async element(element) {
+    const attribute = element.getAttribute;
+    if(attribute) {
+      element.setInnerContent("Michael Jee");
+    }
+  }   
+}
+
+class BodyHandler
+{
+  constructor(attributeName) {
+    this.attributeName = attributeName
+  }
+  
+  async element(element) {
+    const attribute = element.getAttribute;
+    if(attribute) {
+      element.setAttribute("style", "background-color:#44337A");
+    }
+  }   
+}
 
 class LinksHandler
 {
@@ -21,10 +41,10 @@ class LinksHandler
   async element(element) {
     const attribute = element.getAttribute;
     if(attribute) {
-      element.setInnerContent('<a href=\"' + array[0].url + '\"></a> ');
-      element.append('<a href=\"' + array[1].url + '\"></a> ');
-      element.append('<a href=\"' + array[2].url + '\"></a> ');
-      element.prepend('<a href=\"' + array[0].url + '\"></a> ');
+console.log(element.namespaceURI);
+      element.append('<a href=\"' + array[0].url + '\">Wrangler</a>', {html:true});
+      element.append('<a href=\"' + array[1].url + '\">HTML Rewriter</a>', {html:true});
+      element.append('<a href=\"' + array[2].url + '\">Cloudflare Intern Assignment</a>', {html:true});
     }
   }   
 }
@@ -38,7 +58,7 @@ class ProfileHandler
   async element(element) {
     const attribute = element.getAttribute;
     if(attribute) {
-      element.setAttribute("style", "");
+      element.removeAttribute("style");
     }
   }   
 }
@@ -71,28 +91,51 @@ class ProfileNameHandler
   }   
 }
 
+class SocialHandler
+{
+  constructor(attributeName) {
+    this.attributeName = attributeName
+  }
+  
+  async element(element) {
+    const attribute = element.getAttribute;
+    if(attribute) {
+      element.removeAttribute("style");
+      element.append('<a id=\"linkedin\" href=\"https://www.linkedin.com/in/michaeljee2/\">' + 
+        '<img id=\"linkedinsvg\" src=\"https://simpleicons.org/icons/linkedin.svg\" alt=\"linkedin\">' +
+        '</img></a>', {html:true});
+      element.append('<a id=\"github\" href=\"https://github.com/mdjee805/\"><img id=\"githubsvg\"' + 
+        'src=\"https://simpleicons.org/icons/github.svg\" alt=\"github\"></img></a>', {html:true});
+      element.append('<a id=\"rankedVoting\"' + 
+        'href=\"http://ranked-choice-voting-env1.eba-fyvhhdzp.us-west-1.elasticbeanstalk.com/\">' + 
+        '<img id=\"rankedVotingsvg\" src=\"https://simpleicons.org/icons/amazonaws.svg\"' + 
+        'alt=\"rankedVoting\"> </img></a>', {html:true});
+    }
+  }   
+}
+
 const rewriter = new HTMLRewriter()
+  .on('title', new TitleHandler())
+  .on('body', new BodyHandler())
   .on('div#links', new LinksHandler())
   .on('div#profile', new ProfileHandler())
   .on('img#avatar', new ProfilePicHandler())
   .on('h1#name', new ProfileNameHandler())
+  .on('div#social', new SocialHandler())
 
 async function handleRequest(request) {
   if (request.url.split('michaeldanieljee.workers.dev')[1] === '/links') {
-    /*return new Response(array, {
-      headers: { 'content-type': 'application/json' },
-      })*/
-    return new Response(await generateLinkArray(request), {
+    return new Response(await generateLinkArray(array), {
       headers: { 'content-type': 'application/json' },
       })
-  } else {//if (request.url.split('michaeldanieljee.workers.dev')[1] === '/mdjee.png') {
+  } else {
     return await generatePage(request)  }
 }
 
-async function generateLinkArray(request) {
-  generateLink(array, "name0", "https://linkurl/0");
-  generateLink(array, "name1", "https://linkurl/1");
-  generateLink(array, "name2", "https://linkurl/2");
+async function generateLinkArray(array) {
+  generateLink(array, "wrangler", "https://github.com/cloudflare/wrangler");
+  generateLink(array, "htmlRewriter", "https://developers.cloudflare.com/workers/runtime-apis/html-rewriter#properties");
+  generateLink(array, "internAssignment", "https://github.com/cloudflare-hiring/cloudflare-2020-general-engineering-assignment/");
   return JSON.stringify(array);
 }
 
@@ -102,6 +145,7 @@ async function generateLink(array, name, link) {
 }
 
 async function generatePage(request) {
+  await generateLinkArray(array);
   const res = await fetch("https://static-links-page.signalnerve.workers.dev");
   return rewriter.transform(res);
 }
